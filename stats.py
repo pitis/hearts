8a45a586aaa3a89d5504f157ed31be6aadb2dbd4
Pitis <pitis.radu@gmail.com>
2022-02-25 15:30:40 +0200

    stats file

---
+++ stats.py
@@ -0,0 +1,53 @@
+import time
+import board
+import busio
+import digitalio
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+import subprocess
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Display Parameters
+WIDTH = 128
+HEIGHT = 64
+BORDER = 5
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(
+    WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+font = ImageFont.truetype('PixelOperator.ttf', 16)
+#font = ImageFont.load_default()
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
+
+    # Pi Stats Display
+    draw.text((40, 16), "Te iubesc", font=font, fill=255)
+    draw.text((30, 32), "cel mai mult", font=font, fill=255)
+
+    # Display image
+    oled.image(image)
+    oled.show()
+    time.sleep(.1)
