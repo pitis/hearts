7fec34367f0f9aab5c90b14a9aa1380f82570ae5
Pitis <pitis.radu@gmail.com>
2022-02-25 15:51:36 +0200

    feel free

--- stats.py
+++ stats.py
@@ -50,12 +50,9 @@ while True:
     # Display image
     oled.image(image)
     oled.show()
-    bg = Image.new('RGB', (128, 64), 0)
-
     heart1 = Image.open('heart1.gif').convert('1')
-    bg.paste(heart1, (0, 0))
-    bg.save('heart1.png')
+    heart1.resize((128, 64))
 
-    oled.image(Image.open('heart1.png').convert('1'))
+    oled.image(heart1)
     oled.show()
     time.sleep(.1)
